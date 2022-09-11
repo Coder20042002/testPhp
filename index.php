aa5781dee2e0586c7c4d05abc1d7bffb6603c992
Hoai Phuong Tran <phuonghoai090909@gmail.com>
2022-09-11 23:04:59 +0700

    change

--- index.php
+++ index.php
@@ -10,9 +10,121 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 </head>
-
+<style>
+	.header{font-weight: bold; color:white;background-color: black;}
+	.chan{color:black;background-color: #9f9;}
+	.le{color:brown;background-color: #ff9;}
+	.td_quy{padding:4px 0;}
+</style>
 <body>
+	<?php require"module.php"; ?>
+	
+	<table>
+		<?php
+		for($i=0;$i<10;$i++) {
+			echo"<tr>";
+			for($j=0;$j<10;$j++) {
+			echo"<td onclick='alert(".($j+$i*10).")' style='background-color:".makeRGB()."'>".($j+$i*10)."</td>";
+			}
+		}
+		?>
+	</table>
+
+	<table>
+		<tr class="tb">
+			<td>Quý I</td>
+			<td>Quý II</td>
+			<td>Quý III</td>
+			<td>Quý IV</td>
+			
+		</tr>
+		<?php
+		for($i=0;$i<10;$i++) {
+			$cname=($i%2==0)?"chan":"le";
+			echo"
+			<tr class='".$cname."'>
+				<td class='td_quy'>".(100000+$i*100000)."</td>
+				<td class='td_quy'>".(110000+$i*100000)."</td>
+				<td class='td_quy'>".(120000+$i*100000)."</td>
+				<td class='td_quy'>".(130000+$i*100000)."</td>
+			</tr>
+			";
+	
+		}
+		?>
+	</table>
+	<?php
+	
+	function INBANG($a,$b,$c,$d,$e,$f) {
+		echo"
+		<table with='100%' boder='1'> 
+		<tr>
+			<td>STT</td>
+			<td>Tên</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>$a</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>$b</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>$c</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>$d</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td>$e</td>
+		</tr>
+		<tr>
+			<td>6</td>
+			<td>$f</td>
+		</tr>
+	</tr>
+
+	</table>
+		";
+	}
+	INBANG("PHUONG","NAM","HANG","PHƯỢNG","TƯ","BA");
+
+	function InMot($a,$b) {
+		echo"<li><a href='Tinh.php?P=".$a."&Q=".$b."'>$a+$b<";
+
+	}
+
+	function Tong($a,$b) {
+		return $a+$b;
+	}
+	$a=$_GET['P'];
+	$b=$_GET['Q'];
+	
+	echo"Tổng của $a và $b là :".Tong(4,5);
+	echo"<br>";
+	echo(isset($a)?$a :"Biến a chưa đc khởi tạo" );
+	$a=4;echo"<br>";
+	echo(isset($a)?$a :"Biến a chưa đc khởi tạo");
+
+	
+	?>
+	<ul>
+		<?php
+		InMot(3,5);
+		InMot(7,8);
+		InMot(7,5);
+		InMot(32,1);
+
+		?>
+	</ul>
+
+
 	<div class="container">
+
 		<div class="col">
 			<?php include_once "menu.php"
 
---
+++ module.php
@@ -0,0 +1,5 @@
+<?php
+function makeRGB() {
+    return"rgb(".rand(0,255).",".rand(0,255).",".rand(0,255).")";
+}
+?>
